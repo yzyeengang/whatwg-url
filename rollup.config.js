9b30bfe705f684889f8023eb393b3d75b6421fb9
yohane <yzyeengang@foxmail.com>
2024-09-11 14:50:41 +0800

    Add rollup to build

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const nodeResolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const terser = require("@rollup/plugin-terser");
+const babel = require("@rollup/plugin-babel");
+const { resolve } = require("node:path");
+
+/** @type import('rollup').RollupOptions*/
+module.exports = {
+  input: "index.js",
+  output:
+    [
+      {
+        exports: "named",
+        file: "dist/index.esm.js",
+        format: "esm"
+      },
+      {
+        exports: "named",
+        file: "dist/index.cjs.js",
+        format: "cjs"
+      }
+    ],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    terser(),
+    babel({
+      babelHelpers: "bundled",
+      include: [
+        "index.js",
+        "lib/**",
+        /webidl-conversions/u
+      ]
+    })
+  ]
+};
+
+
